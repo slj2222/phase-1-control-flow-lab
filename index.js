bd50951e47a9553232a8a5a8c7281731cb6f7981
Spencer Johnson <sljohnson2222@gmail.com>
2022-01-31 15:11:29 -0600

    Completed Assignment

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(someNumber){
+  let result
+  if (someNumber === 199) {
+    result = "This one is on me!";
+  } else if (someNumber === 2001) {
+      result = "I will gladly take your thirty bucks.";
+    } else if (someNumber === 2501) {
+      result = "No can do.";
+    }
+  return result;
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(someCity){
+  return someCity === 'NYC'?'Ok, sounds good.' : 'No go.';
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+
+function switchOnCharmFromTip(someTip){
+  switch(someTip) {
+    case 'generous':
+      return "Thank you so much."
+    case 'not as generous':
+      return "Thank you."
+    default:
+      return "Bye."
+  }
+  
 }
